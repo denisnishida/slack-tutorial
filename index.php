8a07b75f405782bf6b845d41ed15c3b73c6f770d
Denis T. Nishida <denis.nishida@gmail.com>
2017-01-05 15:19:56 -0600

    Initial code

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * A lightweight example script for demonstrating how to
+ * work with the Slack API.
+ */
+
+// Define Slack application identifiers
+// Even better is to put these in environment variables so you don't risk exposing
+// them to the outer world (e.g. by committing to version control)
+define( 'SLACK_CLIENT_ID', '24016979477.123862040083' );
+define( 'SLACK_CLIENT_SECRET', '22472be49ad8b91132fdc3ec0b2e8a6f' );
+  
+// Include our Slack interface classes
+require_once 'slack-interface/class-slack.php';
+require_once 'slack-interface/class-slack-access.php';
+require_once 'slack-interface/class-slack-api-exception.php';
+ 
+use Slack_Interface\Slack;
+use Slack_Interface\Slack_API_Exception;
+ 
+//
+// HELPER FUNCTIONS
+//
+ 
+    /**
+     * Initializes the Slack handler object, loading the authentication
+     * information from a text file. If the text file is not present,
+     * the Slack handler is initialized in a non-authenticated state.
+     *
+     * @return Slack    The Slack interface object
+     */
+    function initialize_slack_interface() {
+        // Read the access data from a text file
+        if ( file_exists( 'access.txt' ) ) {
+            $access_string = file_get_contents( 'access.txt' );
+        } else {
+            $access_string = '{}';
+        }
+     
+        // Decode the access data into a parameter array
+        $access_data = json_decode( $access_string, true );
+     
+        $slack = new Slack( $access_data );
+         
+        return $slack;
+    }
+ 
+    /**
+     * Executes an application action (e.g. 'send_notification').
+     * 
+     * @param Slack  $slack     The Slack interface object
+     * @param string $action    The id of the action to execute
+     *
+     * @return string   A result message to show to the user
+     */
+    /**
+     * Executes an application action (e.g. 'send_notification').
+     *
+     * @param Slack  $slack     The Slack interface object
+     * @param string $action    The id of the action to execute
+     *
+     * @return string   A result message to show to the user
+     */
+    function do_action( $slack, $action ) {
+        $result_message = '';
+     
+        switch ( $action ) {
+     
+            // Handles the OAuth callback by exchanging the access code to
+            // a valid token and saving it in a file
+            case 'oauth':
+                $code = $_GET['code'];
+     
+                // Exchange code to valid access token
+                try {
+                    $access = $slack->do_oauth( $code );
+                    if ( $access ) {
+                        file_put_contents( 'access.txt', $access->to_json() );
+                        $result_message = 'The application was successfully added to your Slack channel';
+                    }
+                } catch ( Slack_API_Exception $e ) {
+                    $result_message = $e->getMessage();
+                }
+                break;
+     
+            default:
+                break;
+     
+        }
+     
+        return $result_message;
+    }
+ 
+//
+// MAIN FUNCTIONALITY
+//
+ 
+// Setup the Slack interface
+$slack = initialize_slack_interface();
+ 
+// If an action was passed, execute it before rendering the page
+$result_message = '';
+if ( isset( $_REQUEST['action'] ) ) {
+    $action = $_REQUEST['action'];
+    $result_message = do_action( $slack, $action );
+}
+ 
+//
+// PAGE LAYOUT
+//
+ 
+?>
+<html>
+    <head>
+        <title>Slack Integration Example</title>
+         
+        <style>
+            body {
+                font-family: Helvetica, sans-serif;
+                padding: 20px;
+            }
+             
+            .notification {
+                padding: 20px;
+                background-color: #fafad2;
+            }
+ 
+            input {
+                padding: 10px;
+                font-size: 1.2em;
+                width: 100%;
+            }
+        </style>
+    </head>
+     
+    <body>
+        <h1>Slack Integration Example</h1>
+ 
+        <?php if ( $result_message ) : ?>
+            <p class="notice">
+                <?php echo $result_message; ?>
+            </p>
+        <?php endif; ?>
+ 
+        <?php if ( $slack->is_authenticated() ) : ?>
+            <form action="" method="post">
+                <input type="hidden" name="action" value="send_notification"/>
+                <p>
+                    <input type="text" name="text" placeholder="Type your notification here and press enter to send." />
+                </p>
+            </form>
+        <?php else : ?>
+            <p>
+                <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands&client_id=<?php echo $slack->get_client_id(); ?>"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>
+            </p>
+        <?php endif; ?>
+    </body>
+</html>
---
+++ slack-interface/class-slack.php
@@ -0,0 +1,127 @@
+<?php
+namespace Slack_Interface;
+
+require_once 'Requests-master/library/Requests.php';
+ 
+/**
+ * A basic Slack interface you can use as a starting point
+ * for your own Slack projects.
+ */
+class Slack {
+ 
+    private static $api_root = 'https://slack.com/api/';
+ 
+    /**
+	 * @var Slack_Access    Slack authorization data
+	 */
+	private $access;
+	 
+	/**
+	 * Sets up the Slack interface object.
+	 *
+	 * @param array $access_data An associative array containing OAuth
+	 *                           authentication information. If the user
+	 *                           is not yet authenticated, pass an empty array.
+	 */
+	public function __construct( $access_data ) {
+	    if ( $access_data ) {
+	        $this->access = new Slack_Access( $access_data );
+	    }
+	}
+
+	/**
+	 * Checks if the Slack interface was initialized with authorization data.
+	 *
+	 * @return bool True if authentication data is present. Otherwise false.
+	 */
+	public function is_authenticated() {
+	    return isset( $this->access ) && $this->access->is_configured();
+	}
+
+    /**
+	 * Returns the Slack client ID.
+	 * 
+	 * @return string   The client ID or empty string if not configured 
+	 */
+	public function get_client_id() {
+	    // First, check if client ID is defined in a constant
+	    if ( defined( 'SLACK_CLIENT_ID' ) ) {
+	        return SLACK_CLIENT_ID;
+	    }
+	 
+	    // If no constant found, look for environment variable
+	    if ( getenv( 'SLACK_CLIENT_ID' ) ) {
+	        return getenv( 'SLACK_CLIENT_ID' );
+	    }
+	         
+	    // Not configured, return empty string
+	    return '';
+	}
+	 
+	/**
+	 * Returns the Slack client secret.
+	 * 
+	 * @return string   The client secret or empty string if not configured
+	 */
+	private function get_client_secret() {
+	    // First, check if client secret is defined in a constant
+	    if ( defined( 'SLACK_CLIENT_SECRET' ) ) {
+	        return SLACK_CLIENT_SECRET;
+	    }
+	 
+	    // If no constant found, look for environment variable
+	    if ( getenv( 'SLACK_CLIENT_SECRET' ) ) {
+	        return getenv( 'SLACK_CLIENT_SECRET' );
+	    }
+	 
+	    // Not configured, return empty string
+	    return '';
+	}
+
+	/**
+	 * Completes the OAuth authentication flow by exchanging the received
+	 * authentication code to actual authentication data.
+	 *
+	 * @param string $code  Authentication code sent to the OAuth callback function
+	 *
+	 * @return bool|Slack_Access    An access object with the authentication data in place
+	 *                              if the authentication flow was completed successfully.
+	 *                              Otherwise false.
+	 *
+	 * @throws Slack_API_Exception 
+	 */
+	public function do_oauth( $code ) {
+	    // Set up the request headers
+	    $headers = array( 'Accept' => 'application/json' );
+	         
+	    // Add the application id and secret to authenticate the request
+	    $options = array( 'auth' => array( $this->get_client_id(), $this->get_client_secret() ) );
+	 
+	    // Add the one-time token to request parameters 
+	    $data = array( 'code' => $code );
+	 
+	    $response = Requests::post( self::$api_root . 'oauth.access', $headers, $data, $options );
+	         
+	    // Handle the JSON response
+	    $json_response = json_decode( $response->body );
+	 
+	    if ( ! $json_response->ok ) {
+	        // There was an error in the request
+	        throw new Slack_API_Exception( $json_response->error );
+	    }
+	 
+	    // The action was completed successfully, store and return access data
+	    $this->access = new Slack_Access(
+	        array(
+	            'access_token' => $json_response->access_token,
+	            'scope' => explode( ',', $json_response->scope ),
+	            'team_name' => $json_response->team_name,
+	            'team_id' => $json_response->team_id,
+	            'incoming_webhook' => $json_response->incoming_webhook
+	        )
+	    );
+	 
+	    return $this->access;
+	}
+ 
+}
