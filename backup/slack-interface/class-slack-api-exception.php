538cac30e4ddf33dbe6f9d7cc7434e7021bdadbe
Denis T. Nishida <denis.nishida@gmail.com>
2017-01-09 12:52:49 -0600

    Substituded files for the original project files

--- backup/slack-interface/class-slack-api-exception.php
+++ backup/slack-interface/class-slack-api-exception.php
@@ -1,13 +1,11 @@
 <?php
 namespace Slack_Interface;
-
+ 
 use Exception;
-
+ 
 /**
  * A simple exception class for throwing Slack API errors.
- *
- * @author Jarkko Laine <jarkko@jarkkolaine.com>
  */
 class Slack_API_Exception extends Exception {
-
+ 
 }
